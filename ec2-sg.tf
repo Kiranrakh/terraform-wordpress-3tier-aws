a12aac140f90f12f955bf6b1ee786727909d31f6
Kiran Barikrao Rakh <124512771+Kiranrakh@users.noreply.github.com>
2025-06-23 12:03:57 +0530

    ec2-sg.tf

---
+++ ec2-sg.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "kiran_db_sg" {
+  name   = "kiran-db-sg"
+  vpc_id = aws_vpc.kiran_vpc.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.kiran_web_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
